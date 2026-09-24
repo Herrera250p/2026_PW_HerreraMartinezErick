69d35fbf2f6252ef3442acfed593eefc780301de
Herrera250P <eherreram2402@alumno.ipn.mx>
2026-09-24 16:39:25 -0600

    Se agrego values y entries

--- Primer_parcial/02Ejercicios/js/app.js
+++ Primer_parcial/02Ejercicios/js/app.js
@@ -101,8 +101,12 @@ formObjeto.addEventListener('submit', (evento) => {
             resultado = JSON.stringify(Object.keys(taller));
             break;
         case 'values' :
+            resultado = JSON.stringify(Object.values(taller));
             break;
         case 'entries':
+            resultado = Object.entries(taller)
+            .map(([campo, valor]) => `${campo}: ${valor}: `)
+            .join('\n');
             break;
         case 'stringify':
             const textoJson = JSON.stringify(taller, null, 2);
